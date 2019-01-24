6df0b2b32b4a038815a4be66e71655d3ad15ccc4
David Elliott <david.elliott@justice.gov.uk>
2019-01-24 15:35:09 +0000

    Replace guard-rake with guard-rspec
    Replaced to enable only relevant specs to run rather than running
    everything. Also removed coverage from tests when run with guard.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start unless ENV['NO_COVERAGE']
 SimpleCov.minimum_coverage 100
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
