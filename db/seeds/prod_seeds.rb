df387d66b7c9fcb13e6cd5a0ae86b91209cf1a67
David Elliott <david.elliott@justice.gov.uk>
2019-02-14 14:01:01 +0000

    Remove temp password

--- db/seeds/prod_seeds.rb
+++ db/seeds/prod_seeds.rb
@@ -7,7 +7,7 @@ puts 'Creating production seed data...'
 puts 'Creating admin user'
 User.create!(
   email: 'laa-great-ideas@digital.justice.gov.uk',
-  password: 'change_me',
+  password: SecureRandom.hex(10),
   admin: true,
   confirmed_at: Time.now
 )
