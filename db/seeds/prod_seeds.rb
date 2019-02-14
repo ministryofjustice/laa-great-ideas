2667ac0dc9b9b16fd010843a42cdbd3d4b602564
David Elliott <david.elliott@justice.gov.uk>
2019-02-14 14:07:27 +0000

    Find or replace admin so can run with every deployment

--- db/seeds/prod_seeds.rb
+++ db/seeds/prod_seeds.rb
@@ -5,9 +5,8 @@ require 'faker'
 puts 'Creating production seed data...'
 
 puts 'Creating admin user'
-User.create!(
-  email: 'laa-great-ideas@digital.justice.gov.uk',
-  password: SecureRandom.hex(10),
-  admin: true,
-  confirmed_at: Time.now
-)
+User.find_or_create_by!(email: 'laa-great-ideas@digital.justice.gov.uk') do |user|
+  user.password = SecureRandom.hex(10)
+  user.admin = true
+  user.confirmed_at = Time.now
+end
